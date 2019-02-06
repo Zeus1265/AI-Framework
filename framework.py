a94942b6f4b5dcf050e748b96d702a241208f08a
Preston Mouw <prestonmouw@gmail.com>
2019-02-06 15:20:07 -0500

    Pygame testing added

--- framework.py
+++ framework.py
@@ -1,5 +1,29 @@
-```
+'''
 AI Framework
 Version 0.1
 Authors: Sean McGee and Preston Mouw
-```
+'''
+import pygame
+
+pygame.init()
+
+size = (1500, 1000)
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("Very bad demo")
+
+BLUE = pygame.Color(0, 0, 255, 0)
+RED = pygame.Color(255, 0, 0, 0)
+pygame.draw.circle(screen, RED, (750, 500), 100, 0)
+pygame.draw.rect(screen, BLUE, pygame.Rect(0, 0, 1500, 1000), 0)
+pygame.display.update()
+
+pygame.time.delay(1500)
+
+pygame.quit()
+
+'''
+Change log
+Ver 0.1:
+Created file and added basic pygame structure
+'''
