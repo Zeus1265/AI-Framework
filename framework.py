b1eb6c0d63a54a110c3fc310bb56865077edce40
Preston Mouw <prestonmouw@gmail.com>
2019-02-09 22:55:47 -0500

    Map drawing added

--- framework.py
+++ framework.py
@@ -7,6 +7,14 @@ import pygame
 
 pygame.init()
 
+map_t = [[ 1, 1, 1, 1],
+	 [ 1, 0, 0, 1],
+	 [ 1, 0, 0, 1],
+	 [ 1, 1, 1, 1]]
+COL = 4
+ROW = 4
+TILE_SZ = 16
+
 size = (1500, 1000)
 screen = pygame.display.set_mode(size)
 
@@ -14,16 +22,26 @@ pygame.display.set_caption("Very bad demo")
 
 BLUE = pygame.Color(0, 0, 255, 0)
 RED = pygame.Color(255, 0, 0, 0)
-pygame.draw.circle(screen, RED, (750, 500), 100, 0)
-pygame.draw.rect(screen, BLUE, pygame.Rect(0, 0, 1500, 1000), 0)
+
+for r in range(0, ROW):
+	for c in range(0, col):
+		if map_t[r, c] is 1:
+			color = RED
+		else:
+			color = BLUE
+		pygame.draw.rect(screen, RED, pygame.Rect(r*TILE_SZ, c*TILE_SZ, TILE_SZ, TILE_SZ), 0)
+
+
 pygame.display.update()
 
-pygame.time.delay(1500)
+pygame.wait(1500)
 
 pygame.quit()
 
 '''
 Change log
+Ver 0.2:
+Adding map functionailty
 Ver 0.1:
 Created file and added basic pygame structure
 '''
