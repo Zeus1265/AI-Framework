7faf2a12be003dd6a9c3bc4a47065c83eb033f6b
Preston M <45641189+Zeus1265@users.noreply.github.com>
2019-02-10 21:12:35 -0500

    Add files via upload

--- framework.py
+++ framework.py
@@ -7,39 +7,121 @@ import pygame
 
 pygame.init()
 
-map_t = [[ 1, 1, 1, 1],
-	 [ 1, 0, 0, 1],
-	 [ 1, 0, 0, 1],
-	 [ 1, 1, 1, 1]]
-COL = 4
-ROW = 4
-TILE_SZ = 16
+COL = 16
+ROW = 16
 
-size = (1500, 1000)
+map_t = [[ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+         [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [ 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+TILE_SZ = 32
+
+size = (1200, 800)
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Very bad demo")
 
 BLUE = pygame.Color(0, 0, 255, 0)
 RED = pygame.Color(255, 0, 0, 0)
+GREY = pygame.Color(90, 90, 90, 0)
+WHITE = pygame.Color(255, 255, 255, 0)
+GREEN = pygame.Color(0, 255, 0, 0)
 
-for r in range(0, ROW):
-	for c in range(0, col):
-		if map_t[r, c] is 1:
-			color = RED
-		else:
-			color = BLUE
-		pygame.draw.rect(screen, RED, pygame.Rect(r*TILE_SZ, c*TILE_SZ, TILE_SZ, TILE_SZ), 0)
+portal = [0, 0]
 
+running = 1
+player_x = 1
+player_y = 1
+human_input = 1
 
-pygame.display.update()
+REFRESH_RATE = 8 #how many times the game checks for input and updates per sec
+pause = (int)(1000.0/REFRESH_RATE)
 
-pygame.wait(1500)
-
-pygame.quit()
+move_type = 4 
+#0: up
+#1: down
+#2: left
+#3: right
+#4: wait
+while(running):
+    
+    for r in range(0, ROW):
+        for c in range(0, COL):
+            if map_t[r][c] is 1:
+                color = RED
+            elif map_t[r][c] is 0:
+                color = BLUE
+            elif map_t[r][c] is 2:
+                color = WHITE
+                portal = [r, c]
+            #print('Drawing rectangle at {}, {}'.format(r, c))
+            pygame.draw.rect(screen, color, pygame.Rect(r*TILE_SZ, c*TILE_SZ, TILE_SZ, TILE_SZ), 0)
+            pygame.draw.rect(screen, GREY, pygame.Rect(r*TILE_SZ, c*TILE_SZ, TILE_SZ, TILE_SZ), 1)
+    pygame.draw.rect(screen, GREEN, pygame.Rect(player_y * TILE_SZ, player_x * TILE_SZ, TILE_SZ, TILE_SZ), 0)
+    #print('portal at {}'.format(portal))
+    pygame.display.update()
+    
+    if portal[0] is player_y and portal[1] is player_x:
+        pygame.quit()
+        running = 0
+    
+    for event in pygame.event.get():
+        if event.type is pygame.QUIT:
+            pygame.quit()
+            running = 0
+        elif (event.type is pygame.KEYDOWN) and (human_input is 1):
+            keys = pygame.key.get_pressed()
+            #print(event.key)
+            if keys[pygame.K_UP]:
+                move_type = 0
+                #print('UP KEY PRESSED')
+            elif keys[pygame.K_DOWN]:
+                move_type = 1
+                #print('DOWN KEY PRESSED')
+            elif keys[pygame.K_LEFT]:
+                move_type = 2
+                #print('LEFT KEY PRESSED')
+            elif keys[pygame.K_RIGHT]:
+                move_type = 3
+                #print('RIGHT KEY PRESSED')
+        elif (event.type is pygame.KEYUP) and (human_input is 1):
+            move_type = 4
+    
+    if move_type is 0:
+        player_x -= 1
+        if map_t[player_x][player_y] is 1:
+            player_x += 1
+    elif move_type is 1:
+        player_x += 1
+        if map_t[player_x][player_y] is 1:
+            player_x -= 1
+    elif move_type is 2:
+        player_y -= 1
+        if map_t[player_x][player_y] is 1:
+            player_y += 1
+    elif move_type is 3:
+        player_y += 1
+        if map_t[player_x][player_y] is 1:
+            player_y -= 1
+    pygame.time.wait(pause)
 
 '''
 Change log
+Ver 0.3:
+Added basic human input
 Ver 0.2:
 Adding map functionailty
 Ver 0.1:
