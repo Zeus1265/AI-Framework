5428bfa186b4e2258085373f232bb1e2db1ab769
Ares1265 <smcgee2@gulls.salisbury.edu>
2019-02-10 22:56:33 -0500

    DecisionFactory

--- framework.py
+++ framework.py
@@ -1,9 +1,26 @@
 '''
 AI Framework
-Version 0.1
+Version 0.4
 Authors: Sean McGee and Preston Mouw
 '''
 import pygame
+from DecisionFactory import *
+
+#asks user if df will play or human will play
+
+player = raw_input("Who will play? (H)uman or (D)ecisionFactory")
+
+if player is 'H' or player is 'h':
+	human_input = 1
+	REFRESH_RATE = 8 #how many times the game checks for input and updates per sec
+else:
+	human_input = 0
+	REFRESH_RATE = 60
+	moves = 0
+	Self.DF = DecisionFactory.DecisionFactory()
+
+pause = (int)(1000.0/REFRESH_RATE)
+
 
 pygame.init()
 
@@ -32,7 +49,7 @@ TILE_SZ = 32
 size = (1200, 800)
 screen = pygame.display.set_mode(size)
 
-pygame.display.set_caption("Very bad demo")
+pygame.display.set_caption("Preston-ta-Sean Framework")
 
 BLUE = pygame.Color(0, 0, 255, 0)
 RED = pygame.Color(255, 0, 0, 0)
@@ -45,19 +62,15 @@ portal = [0, 0]
 running = 1
 player_x = 1
 player_y = 1
-human_input = 1
 
-REFRESH_RATE = 8 #how many times the game checks for input and updates per sec
-pause = (int)(1000.0/REFRESH_RATE)
-
-move_type = 4 
+move_type = 4
 #0: up
 #1: down
 #2: left
 #3: right
 #4: wait
 while(running):
-    
+
     for r in range(0, ROW):
         for c in range(0, COL):
             if map_t[r][c] is 1:
@@ -73,53 +86,83 @@ while(running):
     pygame.draw.rect(screen, GREEN, pygame.Rect(player_y * TILE_SZ, player_x * TILE_SZ, TILE_SZ, TILE_SZ), 0)
     #print('portal at {}'.format(portal))
     pygame.display.update()
-    
+
     if portal[0] is player_y and portal[1] is player_x:
         pygame.quit()
+        if human_input is 0:
+			print moves
         running = 0
-    
-    for event in pygame.event.get():
-        if event.type is pygame.QUIT:
-            pygame.quit()
-            running = 0
-        elif (event.type is pygame.KEYDOWN) and (human_input is 1):
-            keys = pygame.key.get_pressed()
-            #print(event.key)
-            if keys[pygame.K_UP]:
-                move_type = 0
-                #print('UP KEY PRESSED')
-            elif keys[pygame.K_DOWN]:
-                move_type = 1
-                #print('DOWN KEY PRESSED')
-            elif keys[pygame.K_LEFT]:
-                move_type = 2
-                #print('LEFT KEY PRESSED')
-            elif keys[pygame.K_RIGHT]:
-                move_type = 3
-                #print('RIGHT KEY PRESSED')
-        elif (event.type is pygame.KEYUP) and (human_input is 1):
-            move_type = 4
-    
-    if move_type is 0:
-        player_x -= 1
+
+	if human_input is 1:
+	    for event in pygame.event.get():
+	        if event.type is pygame.QUIT:
+	            pygame.quit()
+	            running = 0
+	        elif (event.type is pygame.KEYDOWN):
+	            keys = pygame.key.get_pressed()
+	            #print(event.key)
+	            if keys[pygame.K_UP]:
+	                move_type = 0
+	                #print('UP KEY PRESSED')
+	            elif keys[pygame.K_DOWN]:
+	                move_type = 1
+	                #print('DOWN KEY PRESSED')
+	            elif keys[pygame.K_LEFT]:
+	                move_type = 2
+	                #print('LEFT KEY PRESSED')
+	            elif keys[pygame.K_RIGHT]:
+	                move_type = 3
+	                #print('RIGHT KEY PRESSED')
+	        elif (event.type is pygame.KEYUP):
+	            move_type = 4
+	else:
+		move_type = Self.DF.get_decision()
+		# This is decremented because in the DecisionFactory code 0 is the wait
+		# decision so this is to compensate and align with the human input code
+		move_type -= 1
+
+	if move_type is 0:
+			player_x -= 1
         if map_t[player_x][player_y] is 1:
             player_x += 1
+            result = "Wall"
+        else:
+			result = "Success"
+
     elif move_type is 1:
         player_x += 1
         if map_t[player_x][player_y] is 1:
             player_x -= 1
+            result = "Wall"
+        else:
+			result = "Success"
+
     elif move_type is 2:
         player_y -= 1
         if map_t[player_x][player_y] is 1:
             player_y += 1
+            result = "Wall"
+        else:
+			result = "Success"
+
     elif move_type is 3:
         player_y += 1
         if map_t[player_x][player_y] is 1:
             player_y -= 1
+            result = "Wall"
+        else:
+			result = "Success"
+
+	if human_input is 0:
+		Self.DF.put_result(result)
+		moves += 1
+
     pygame.time.wait(pause)
 
 '''
 Change log
+Ver 0.4:
+Added ability to use df or human input
 Ver 0.3:
 Added basic human input
 Ver 0.2:
